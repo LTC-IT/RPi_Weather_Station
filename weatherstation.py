656763794820c5cbf6f50f2fdff1793af4f2cc16
Ryan Cather <ryan.cather@ed.act.edu.au>
2019-04-30 10:28:43 +0000

    calculated the level to show on the thermometer (but not show it yet)

--- weatherstation.py
+++ weatherstation.py
@@ -1,41 +1,9 @@
-from sense_emu import SenseHat
-from time import sleep
+40/7 = 5.7
 
-sense = SenseHat()
-
-b = [0,0,0]
-w = [255,255,255]
-r = [255,0,0]
-
-max = 40
-min = 0
-
-temp_range = [False, False, False, False, False, False, False]
-
-temp_level = round(sense.get_temperature() / 7)
-
-print (temp_level)
-
-for index in range (0,temp_level+1):
-    temp_range[index] = True
-    
-print(temp_range)
-
-thermometer = [
-    b,b,w,b,b,w,b,b,
-    b,b,w,w,b,w,b,b,
-    b,b,w,b,b,w,b,b,
-    b,b,w,w,b,w,b,b,
-    b,b,w,b,b,w,b,b,
-    b,b,w,w,b,w,w,b,
-    b,b,w,b,b,b,w,b,
-    b,b,w,w,w,w,w,b
-    ]
-
-sense.set_pixels(thermometer)
-
-#while True:
-    #sense.show_message("Humidity : {}%".format(round(sense.get_humidity())))
-    #sense.show_message("Temperature: {} celcius".format(round(sense.get_temperature())))
-    #sense.show_message("Pressure: {} mbar".format(round(sense.get_pressure())))
-    #sleep(1)
+0-5
+6-11
+12-17
+18-23
+24-29
+30-35
+36-40
