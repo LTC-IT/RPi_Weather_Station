dd3f8cf1ebb9734ad9fc9dc29bf689b383746719
LTC-IT <49006682+LTC-IT@users.noreply.github.com>
2019-05-07 08:59:57 +1000

    Add files via upload

--- weatherstation.py
+++ weatherstation.py
@@ -7,61 +7,75 @@ b = [0,0,0]
 w = [255,255,255]
 r = [255,0,0]
 
-max = 40
-min = 0
+def temperature_display():
+    #TODO Scale the values to range between min and max.
 
-temp_range = [False, False, False, False, False, False, False]
+    max = 40
+    min = 0
 
-temp_level = round(sense.get_temperature() / 7)
+    temp_range = [False, False, False, False, False, False, False]
 
-print (temp_level)
 
-# Changes the elements to true up to temp_level
-for index in range (6,temp_level,-1):
-    temp_range[index] = True
+
+    temp_level = round(sense.get_temperature() / 7)
+
+    print (temp_level)
+
+    # Changes the elements to true up to temp_level
+    #for index in range (6,temp_level,-1):
+    for counter in range (0,temp_level):
+        temp_range[6-counter] = True
+        
+    print(temp_range)
+
+    thermometer = [
+        b,b,w,b,b,w,b,b,
+        b,b,w,w,b,w,b,b,
+        b,b,w,b,b,w,b,b,
+        b,b,w,w,b,w,b,b,
+        b,b,w,b,b,w,b,b,
+        b,b,w,w,b,w,w,b,
+        b,b,w,b,b,b,w,b,
+        b,b,w,w,w,w,w,b
+        ]
+
+    if temp_range[0] == True:
+        thermometer[3] = [255,0,0]
+        thermometer[4] = [255,0,0]
+
+    if temp_range[1] == True:
+        thermometer[12] = [255,0,0]
+        
+    if temp_range[2] == True:
+        thermometer[19] = [255,0,0]
+        thermometer[20] = [255,0,0]
+
+    if temp_range[3] == True:
+        thermometer[28] = [255,0,0]
+
+    if temp_range[4] == True:
+        thermometer[35] = [255,0,0]
+        thermometer[36] = [255,0,0]
+
+    if temp_range[5] == True:
+        thermometer[44] = [255,0,0]
+
+    if temp_range[6] == True:
+        thermometer[51] = [255,0,0]
+        thermometer[52] = [255,0,0]
+        thermometer[53] = [255,0,0]
+
+    sense.set_pixels(thermometer)
+
+
+def humidity_display():
+    humidty = round(sense.get_humidity()))
     
-print(temp_range)
+    
 
-thermometer = [
-    b,b,w,b,b,w,b,b,
-    b,b,w,w,b,w,b,b,
-    b,b,w,b,b,w,b,b,
-    b,b,w,w,b,w,b,b,
-    b,b,w,b,b,w,b,b,
-    b,b,w,w,b,w,w,b,
-    b,b,w,b,b,b,w,b,
-    b,b,w,w,w,w,w,b
-    ]
-
-sense.set_pixels(thermometer)
-
-'''
-# Version 1 - Manual
-# This doesn't use the list.
-
-for row in range (temp_level, 0, -1):
-    row_number = row * 8 + 4
-    print(row_number)
-    thermometer[row_number] = [255,0,0]
-
-if temp_level >= 0:
-    thermometer[51] = [255,0,0]
-    thermometer[52] = [255,0,0]
-    thermometer[53] = [255,0,0]
-
-for row in range (temp_level, 0, -1):
-    if temp_level >=2:
-        row_number = row * 8 + 3
-        if thermometer[row_number] == [0,0,0]:
-            thermometer[row_number] = [255,0,0]
-
-'''
-# TODO: Version 2- 
-
-sense.set_pixels(thermometer)
-
-#while True:
+while True:
+    temperature_display()
     #sense.show_message("Humidity : {}%".format(round(sense.get_humidity())))
     #sense.show_message("Temperature: {} celcius".format(round(sense.get_temperature())))
     #sense.show_message("Pressure: {} mbar".format(round(sense.get_pressure())))
-    #sleep(1)
+    sleep(0.5)
